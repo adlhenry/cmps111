2e14153f572937f994c66b745cf1311ffbe86053
adlhenry <adlhenry@ucsc.edu>
2016-01-17 15:04:11 -0800

    Upload assignment 1

---
+++ asgn1/myshell.c
@@ -0,0 +1,38 @@
+// Author: Adam Henry, adlhenry@ucsc.edu
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include <libgen.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+
+extern char **getLine();
+
+int exit_status = EXIT_SUCCESS;
+char *execname = NULL;
+
+void errprintf (char *string) {
+	fflush(NULL);
+	fprintf(stderr, "%s: ", execname);
+	if (string != NULL) fprintf(stderr, "%s\n", string);
+	fflush(NULL);
+	exit_status = EXIT_FAILURE;
+}
+
+int main (int argc, char *argv[]) {
+	execname = basename(argv[0]);
+	
+	int i;
+	char **args;
+	
+	while (TRUE) {
+		args = getLine();
+		for (i = 0; args[i] != NULL; i++) {
+			printf("Argument %d: %s\n", i, args[i]);
+		}
+	}
+	
+	return exit_status;
+}
