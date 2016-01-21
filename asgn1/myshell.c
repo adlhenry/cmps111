233b9d1ca9a2716e17028181f3b06bc8b63d7674
adlhenry <adlhenry@ucsc.edu>
2016-01-21 15:32:23 -0800

    Refactor

--- asgn1/myshell.c
+++ asgn1/myshell.c
@@ -16,6 +16,10 @@ extern char **getLine();
 int exit_status = EXIT_SUCCESS;
 char *execname = NULL;
 
+char *outfile = NULL;
+char *infile = NULL;
+char **argvs[10];
+
 // Print a warning to stderr
 void errprintf (char *string) {
 	fflush(NULL);
@@ -30,10 +34,10 @@ void print_prompt () {
 	printf("$> ");
 }
 
-char *outfile = NULL;
-char *infile = NULL;
-char **argvs[10];
-char ***getLine2 () {
+// Parse the command line
+void read_command () {
+	outfile = NULL;
+	infile = NULL;
 	char **args = getLine();
 	argvs[0] = args;
 	int j = 0;
@@ -53,7 +57,6 @@ char ***getLine2 () {
 				break;
 		}
 	}
-	return argvs;
 }
 
 /*
@@ -74,20 +77,21 @@ void output_redirect_close (int fd) {
 }
 */
 
-void redirect_out (char *filename) {
-	if (filename == NULL) return;
-	close(STDOUT_FILENO);
-	int fd = creat(filename, 00644);
-	if (fd == -1) perror(filename);
+// Redirect stdout and stdin
+void redirect () {
+	if (outfile != NULL) {
+		close(STDOUT_FILENO);
+		int fd = creat(outfile, 00644);
+		if (fd == -1) perror(outfile);
+	}
+	if (infile != NULL) {
+		close(STDIN_FILENO);
+		int fd = open(infile, O_RDONLY);
+		if (fd == -1) perror(infile);
+	}
 }
 
-void redirect_in (char *filename) {
-	if (filename == NULL) return;
-	close(STDIN_FILENO);
-	int fd = open(filename, O_RDONLY);
-	if (fd == -1) perror(filename);
-}
-
+// Check for exit command
 bool is_exit (char **args) {
 	if (strcmp(args[0], "exit") == 0) {
 		if (args[1] != NULL) {
@@ -107,18 +111,13 @@ int main (int argc, char *argv[]) {
 		return exit_status;
 	}
 	
-	char *command = NULL;
-	char **parameters = NULL;
-	int status;
-	
 	while (true) {
-		print_prompt();
+		print_prompt();		
+		read_command();
 		
-		outfile = NULL;
-		infile = NULL;
-		char ***argvs = getLine2();
-		parameters = argvs[0];
-		command = parameters[0];
+		int status;
+		char **parameters = argvs[0];
+		char *command = parameters[0];
 		
 		if (is_exit(parameters)) continue;
 		
@@ -131,15 +130,13 @@ int main (int argc, char *argv[]) {
 			if (ret == -1) perror(execname);
 		} else {
 			// Child code
-			redirect_out(outfile);
-			redirect_in(infile);
+			redirect();
 			int ret = execvp(command, parameters);
 			if (ret == -1) {
 				perror(command);
 				exit(exit_status);
 			}
 		}
-
 	}
 	
 	return exit_status;
